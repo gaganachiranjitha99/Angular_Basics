75c6d4d52ec2d993764c6f325589354dc2f44266
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-17 21:14:47 +0530

    updated

--- Angular-Basics/src/app/post/post.component.ts
+++ Angular-Basics/src/app/post/post.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+
+  constructor(){}
+
+
+  ngOnInit(): void {
+
+    
+    
+  }
 
 }
