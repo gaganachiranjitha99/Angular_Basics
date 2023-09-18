dae90ec4c9f821b7bfd659e3d9daa6afcfa72b64
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-18 19:03:18 +0530

    after view init

--- Angular-Basics/src/app/app.component.ts
+++ Angular-Basics/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component,ViewChild } from '@angular/core';
+import { Component,ViewChild ,AfterViewInit} from '@angular/core';
 import { PostComponent } from './post/post.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Angular-Basics';
   parentMessage:string='message from parent changed';
 
@@ -13,6 +13,10 @@ export class AppComponent {
   @ViewChild(PostComponent) childComp;
   constructor(){
 
+    console.log(this.childComp);
+
+  }
+  ngAfterViewInit(){
     console.log(this.childComp);
 
   }
