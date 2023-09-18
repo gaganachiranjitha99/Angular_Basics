a0bbe17bdcc612768fa88396fab297759b2b9942
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-18 18:15:40 +0530

    contructor call in child component

--- Angular-Basics/src/app/postlist/postlist.component.ts
+++ Angular-Basics/src/app/postlist/postlist.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-postlist',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./postlist.component.css']
 })
 export class PostlistComponent {
+
+
+
+
+  constructor(){
+    
+  }
 
 }
