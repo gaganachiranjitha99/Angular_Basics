68faa709618cce68b6eeb33274486dc061c90fb5
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-10-04 12:40:29 +0530

    post form using angular frontend

--- Angular-Basics/src/app/app.component.ts
+++ Angular-Basics/src/app/app.component.ts
@@ -1,41 +1,20 @@
-import { Component,ViewChild ,AfterViewInit} from '@angular/core';
+import { Component} from '@angular/core';
 import { PostComponent } from './post/post.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  title = 'Angular-Basics';
-  parentMessage:string='message from parent changed';
-  message!: string;
-  fromChildOutput!: string;
-  imgUrl="https://media.licdn.com/dms/image/D5603AQHbHGpprXuzAA/profile-displayphoto-shrink_800_800/0/1692292973588?e=2147483647&v=beta&t=VZeE63--wzywe3SogI-GdFI4Obkp5jnNVl_6kJXwJV0";
-  bool:boolean=true;
-  userName!: string;
+export class AppComponent  {
+  
 
-  @ViewChild(PostComponent) childComp: any;// to get data from child to parent
-  constructor(){
+ 
+  constructor(){}
 
-    console.log(this.childComp);
+    
+  
+  
 
-  }
-  ngAfterViewInit(){
-    console.log(this.childComp);
-    this.message=this.childComp;
-
-  }
-  receiveMessage($event: string){
-
-    this.fromChildOutput=$event;
-  }
-
-  buttonClick(){
-    console.log('button click event works')
-  }
-
-  onKeyup(){
-    console.log(this.userName);
-  }
+ 
 
 }
