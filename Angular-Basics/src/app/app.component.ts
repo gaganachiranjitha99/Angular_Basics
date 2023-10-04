50c5b4fc248789e933bc374dded6047fd88f8ee8
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-10-04 09:26:26 +0530

    PROPERTY BINDING IN NG

--- Angular-Basics/src/app/app.component.ts
+++ Angular-Basics/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent implements AfterViewInit {
   parentMessage:string='message from parent changed';
   message!: string;
   fromChildOutput!: string;
+  imgUrl="https://media.licdn.com/dms/image/D5603AQHbHGpprXuzAA/profile-displayphoto-shrink_800_800/0/1692292973588?e=2147483647&v=beta&t=VZeE63--wzywe3SogI-GdFI4Obkp5jnNVl_6kJXwJV0";
 
   @ViewChild(PostComponent) childComp: any;// to get data from child to parent
   constructor(){
